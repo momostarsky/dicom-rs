db0cd293d4a40057baafa1d87b384904ea1dc3d6
starsky <daihanzhang@gmail.com>
2025-08-18 16:48:51 +0800

    修改解码器

--- parser/src/stateful/decode.rs
+++ parser/src/stateful/decode.rs
@@ -417,7 +417,7 @@ where
                 .buffer
                 .split(|v| *v == b'\\')
                 .map(|slice| {
-                    DefaultCharacterSetCodec
+                    self.text.
                         .decode(slice)
                         .context(DecodeTextSnafu {
                             position: self.position,
